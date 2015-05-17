3b42a0f01cb950ce80474259ffe20cf98d7db460
Alexis Le-Quoc <alq@datadoghq.com>
2015-05-17 19:20:37 -0400

    Got build options

---
+++ build-stap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+make clean
+./configure --enable-shared --prefix=/opt/python --enable-ipv6 --enable-unicode=ucs4 --with-dbmliborder=bdb:gdbm --with-system-expat --with-system-ffi --with-fpectl CC=x86_64-linux-gnu-gcc CFLAGS="-D_FORTIFY_SOURCE=2 -g -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security" --with-dtrace
+make
